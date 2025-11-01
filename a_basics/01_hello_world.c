305f489c5e90946a98ef414d2c533e97383fc6c5
Nervant <rasyidhaidar106@gmail.com>
2025-11-01 22:58:49 +0700

    Create 01_hello_world.c

---
+++ a_basics/01_hello_world.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    // this is my first C program!
+    // printf() displays text to the console
+    
+    printf("Hello World!\n");
+    printf("I love ice cream\n");
+    printf("It's really good!\n");
+
+    return 0;
+}
+
+/*
+ * NOTES:
+ * - #include <stdio.h> gives access to printf()
+ * - main() is where the program starts
+ * - \n creates a new line
+ * - return 0 means the program ran successfully
+ */
